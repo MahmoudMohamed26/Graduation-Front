52fbf6a896f0935216c6a18eee8bfaacf189d8ad
MahmoudMohamed26 <gamesforyou26300@gmail.com>
2025-01-27 03:04:06 +0200

    create project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Login from './Website/Login';
+import { Routes , Route } from 'react-router-dom';
+function App() {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
+}
+
+export default App;
